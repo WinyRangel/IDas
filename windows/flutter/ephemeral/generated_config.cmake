7a26d97371a6ab8727a57ec435015ac68049425b
WinyRangel <99991728+WinyRangel@users.noreply.github.com>
2024-09-11 22:54:47 -0600

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\manza\\Downloads\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\manza\\Documents\\FLUTTER\\mqtt_line_chart-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\manza\\Downloads\\flutter"
+  "PROJECT_DIR=C:\\Users\\manza\\Documents\\FLUTTER\\mqtt_line_chart-main"
+  "FLUTTER_ROOT=C:\\Users\\manza\\Downloads\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\manza\\Documents\\FLUTTER\\mqtt_line_chart-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\manza\\Documents\\FLUTTER\\mqtt_line_chart-main"
+  "FLUTTER_TARGET=C:\\Users\\manza\\Documents\\FLUTTER\\mqtt_line_chart-main\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lZGQ4NTQ2MTE2NDU3YmRmMWM1YmRmYjEzZWNiOTQ2M2QyYmI1ZWQ0Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\manza\\Documents\\FLUTTER\\mqtt_line_chart-main\\.dart_tool\\package_config.json"
+)
